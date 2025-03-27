3fdd62e03253778457f1ff1fa808ff913ebc271a
aleksey <alexx308@mail.ru>
2025-03-27 20:31:46 +0400

    init backend app

---
+++ backend/src/main/java/ru/hh/blokshnote/BlokshnoteApplication.java
@@ -0,0 +1,13 @@
+package ru.hh.blokshnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlokshnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlokshnoteApplication.class, args);
+	}
+
+}
