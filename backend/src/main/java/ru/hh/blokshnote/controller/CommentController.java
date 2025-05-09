ab1dbf2241409cdd3286db86e19b3e12eb9a5993
The_Pimp <mishanin_oleg@bk.ru>
2025-05-09 16:20:19 +0300

    created CommentController and update openapi.json

---
+++ backend/src/main/java/ru/hh/blokshnote/controller/CommentController.java
@@ -0,0 +1,41 @@
+package ru.hh.blokshnote.controller;
+
+import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hh.blokshnote.dto.comment.request.CreateCommentDto;
+import ru.hh.blokshnote.dto.comment.response.CommentDto;
+import ru.hh.blokshnote.dto.comment.response.RoomCommentsResponse;
+import ru.hh.blokshnote.service.CommentService;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class CommentController {
+  private final CommentService commentService;
+
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
+
+  @PostMapping("/{uuid}/comments")
+  public CommentDto createComment(
+      @PathVariable("uuid") UUID roomUuid,
+      @RequestParam("adminToken") UUID adminToken,
+      @RequestBody CreateCommentDto request
+      ) {
+    return commentService.createComment(roomUuid, adminToken, request);
+  }
+
+  @GetMapping("/{uuid}/comments")
+  public RoomCommentsResponse getAllCommentsInRoom(
+      @PathVariable("uuid") UUID roomUuid,
+      @RequestParam("adminToken") UUID adminToken
+  ) {
+    return commentService.getAllCommentsInRoom(roomUuid, adminToken);
+  }
+}
