ed7d5bcce3376113a212bb3a1b885ca18a6dd6d1
Mikhail K <mikhail_kalinin_a@mail.ru>
2025-05-26 22:43:40 +0300

    real llm init

--- backend/src/main/java/ru/hh/blokshnote/service/LlmService.java
+++ backend/src/main/java/ru/hh/blokshnote/service/LlmService.java
@@ -1,5 +1,6 @@
 package ru.hh.blokshnote.service;
 
+import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +8,20 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class LlmService {
+  private final DeepSeekChatModel chatModel;
+
+  public LlmService(DeepSeekChatModel chatModel) {
+    this.chatModel = chatModel;
+  }
+
   @Async
   public CompletableFuture<String> getReviewResponseAsync(String editorText, String prompt) {
-    String response = "LLM response";
+    if (prompt.isBlank()) {
+      prompt = "Read the problem. Analyze the solution and give suggestions.";
+    }
+
+    String combined = prompt + "\n\n" + editorText;
+    String response = chatModel.call(combined);
     return CompletableFuture.completedFuture(response);
   }
 }
